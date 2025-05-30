3d3d373531152f613a72987f1abaf7c67d1a82cf
Edgar Mikuro <edgarmikuro@gmail.com>
2025-05-31 01:34:54 +0300

    Completes React Hooks Forms Lab

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,29 @@ import itemData from "../data/items";
 function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [search, setSearch] = useState("");
 
   function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
+    setIsDarkMode((prev) => !prev);
+  }
+
+  function handleAddItem(newItem) {
+    setItems([...items, newItem]);
+  }
+
+  function handleSearchChange(newSearchValue) {
+    setSearch(newSearchValue);
   }
 
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList
+        items={items}
+        onItemFormSubmit={handleAddItem}
+        search={search}
+        onSearchChange={handleSearchChange}
+      />
     </div>
   );
 }
